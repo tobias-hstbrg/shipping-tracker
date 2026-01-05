fd8a672d1431fefb1e39039e28fd5ee051673c71
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2026-01-05 23:43:00 +0100

    Simple CORS config

---
+++ backend/src/main/java/io/github/tobiashstbrg/shippingtracker/config/WebConfig.java
@@ -0,0 +1,21 @@
+package io.github.tobiashstbrg.shippingtracker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                    .allowedMethods("GET")
+                    .allowedHeaders("*")
+                    .allowCredentials(false);
+    };
+}
+
+
